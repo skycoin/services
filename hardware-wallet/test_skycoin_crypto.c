25853a1b45fa0e263fdb18a494bdc1f61880ccc9
mpsido <mpsido2@gmail.com>
2018-02-12 17:04:30 +0800

    Add tohex function, and try different functions to compute ECDH

--- hardware-wallet/test_skycoin_crypto.c
+++ hardware-wallet/test_skycoin_crypto.c
@@ -6,8 +6,18 @@
 #include "check.h"
 
 #include "ecdsa.h"
+#include "curves.h"
 
 #define FROMHEX_MAXLEN 512
+
+void tohex(char * str, const uint8_t* buffer, int bufferLength)
+{
+	int i;
+	for (i = 0; i < bufferLength; ++i)
+	{
+		sprintf(&str[2*i], "%02x", buffer[i]);
+	}
+}
 
 const uint8_t *fromhex(const char *str)
 {
@@ -54,20 +64,34 @@ START_TEST(test_compute_ecdh)
 {
     char seed_str[256] = "seed";
 	HDNode alice;
-    int key_size;
-    uint8_t session_key1[65] = {0};
+    uint8_t session_key1[32] = {0};
 
     create_node(seed_str, &alice);
 	ck_assert_mem_eq(alice.public_key, fromhex("03008fa0a5668a567cb28ab45e4b6747f5592690c1d519c860f748f6762fa13103"), 33);
 	ck_assert_mem_eq(alice.private_key, fromhex("8f609a12bdfc8572590c66763bb05ce609cc0fdcd0c563067e91c06bfd5f1027"), 32);
 
 
-    hdnode_get_shared_key(&alice, alice.public_key, session_key1, &key_size);
+	char pubkey[66] = {0};
+	tohex(pubkey, alice.public_key, 33);
+	printf("pub key: %s\n", pubkey);
 
 
-	ck_assert_mem_eq(session_key1, fromhex("907d3c524abb561a80644cdb0cf48e6c71ce33ed6a2d5eed40a771bcf86bd081"), 32);
+	// ecdh_multiply(alice.curve->params, alice.private_key, alice.public_key, session_key1); //65
 
-    ck_assert_int_eq(1, 1);
+	curve25519_scalarmult(session_key1, alice.private_key, alice.public_key); // 32
+    // int key_size;
+    // hdnode_get_shared_key(&alice, alice.public_key, session_key1, &key_size); //65
+
+	char key[64] = {0};
+	tohex(key, session_key1, 32);
+	printf("ECDH key: %s\n", key);
+
+	ck_assert_mem_eq(session_key1, fromhex("024f7fd15da6c7fc7d0410d184073ef702104f82452da9b3e3792db01a8b7907c3"), 32);
+
+    uint8_t digest[SHA256_DIGEST_LENGTH] = {0};
+    compute_sha256sum(key, digest, 32);
+
+	ck_assert_mem_eq(digest, fromhex("907d3c524abb561a80644cdb0cf48e6c71ce33ed6a2d5eed40a771bcf86bd081"), 32);
 }
 END_TEST
 
