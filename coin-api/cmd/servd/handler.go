83c81219962278eb4e53da7f942a277c42323358
stgleb <glebstepanov1992@gmail.com>
2018-02-27 18:06:25 +0200

    add handler for check tx status

--- coin-api/cmd/servd/handler.go
+++ coin-api/cmd/servd/handler.go
@@ -2,17 +2,25 @@ package servd
 
 import (
 	"github.com/labstack/echo"
+	"github.com/skycoin/services/coin-api/internal/multi"
+	"github.com/skycoin/skycoin/src/visor"
+	"net/http"
 )
 
 type handlerMulti struct {
+	service *multi.GenericСoinService
 }
 
 type MultiStats struct {
 	Message string `json:"message"`
 }
 
-func newHandlerMulti() *handlerMulti {
-	return &handlerMulti{}
+func newHandlerMulti(nodeAddr string) *handlerMulti {
+	service := multi.NewMultiCoinService(nodeAddr)
+
+	return &handlerMulti{
+		service: service,
+	}
 }
 
 func (h *handlerMulti) generateSeed(e echo.Context) error {
@@ -39,9 +47,32 @@ func (h *handlerMulti) injectTransaction(e echo.Context) error {
 	return nil
 }
 
-func (h *handlerMulti) checkTransaction(e echo.Context) error {
-	//TODO: get request info, call appropriate handler from internal btc, don't pass echo context further
-	// deal with io.Reader interface
+func (h *handlerMulti) checkTransaction(ctx echo.Context) error {
+	txId := ctx.Param("transid")
+	status, err := h.service.CheckTransactionStatus(txId)
+
+	if err != nil {
+		ctx.JSONPretty(http.StatusOK, struct {
+			Status string `json:"status"`
+			Code   int    `json:"code"`
+			Result string `json:"result"`
+		}{
+			"Ok",
+			http.StatusOK,
+			err.Error(),
+		}, "\t")
+	}
+
+	ctx.JSONPretty(http.StatusOK, struct {
+		Status string                  `json:"status"`
+		Code   int                     `json:"code"`
+		Result visor.TransactionStatus `json:"result"`
+	}{
+		"Ok",
+		http.StatusOK,
+		status,
+	}, "\t")
+
 	return nil
 }
 
