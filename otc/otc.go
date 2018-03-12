ec89f8db452b53b1e2005c76677baa3bfa340d19
karl <skycoin.karl@gmail.com>
2018-03-12 09:48:36 -0400

    conf listening ports, config status

--- otc/otc.go
+++ otc/otc.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
@@ -14,21 +15,26 @@ import (
 	"github.com/skycoin/services/otc/pkg/otc"
 )
 
-var CURRENCIES = currencies.New()
+var (
+	CURRENCIES = currencies.New()
+	CONFIG     *otc.Config
+)
 
 func init() {
-	conf, err := otc.NewConfig("config.toml")
+	var err error
+
+	CONFIG, err = otc.NewConfig("config.toml")
 	if err != nil {
 		panic(err)
 	}
 
-	SKY, err := sky.New(conf)
+	SKY, err := sky.New(CONFIG)
 	if err != nil {
 		panic(err)
 	}
 	CURRENCIES.Add(otc.SKY, SKY)
 
-	BTC, err := btc.New(conf)
+	BTC, err := btc.New(CONFIG)
 	if err != nil {
 		panic(err)
 	}
@@ -46,11 +52,12 @@ func main() {
 	}
 
 	admin := admin.New(CURRENCIES, modl)
-	go http.ListenAndServe(":8000", admin)
+	go http.ListenAndServe(CONFIG.API.Admin.Listen, admin)
+	fmt.Printf("api.admin listening at %s\n", CONFIG.API.Admin.Listen)
 
 	public := public.New(CURRENCIES, modl)
-	println("listening")
-	go http.ListenAndServe(":8080", public)
+	go http.ListenAndServe(CONFIG.API.Public.Listen, public)
+	fmt.Printf("api.public listening at %s\n", CONFIG.API.Public.Listen)
 
 	<-stop
 	println("stopping")
