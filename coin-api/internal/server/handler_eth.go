a3df2fd730565f5ae2ffb88f04bafb76e69d01b8
stgleb <glebstepanov1992@gmail.com>
2018-04-19 15:56:57 +0300

    Finish generate keypair handler

--- coin-api/internal/server/handler_eth.go
+++ coin-api/internal/server/handler_eth.go
@@ -3,10 +3,16 @@ package server
 import (
 	"github.com/labstack/echo"
 	"github.com/skycoin/services/coin-api/internal/eth"
+	"net/http"
 )
 
 type handlerEth struct {
 	service *eth.EthService
+}
+
+type ethKeyPairResponse struct {
+	PrivateKey string `json:"private_key"`
+	Address    string `json:"address"`
 }
 
 func NewHandlerEth(nodeUrl string) (*handlerEth, error) {
@@ -21,8 +27,29 @@ func NewHandlerEth(nodeUrl string) (*handlerEth, error) {
 	}, nil
 }
 
-func (h *handlerEth) GenerateKeyPair(ctx *echo.Context) {
+func (h *handlerEth) GenerateKeyPair(ctx echo.Context) error {
+	privateKey, address, err := h.service.GenerateKeyPair()
 
+	if err != nil {
+		handleError(ctx, err)
+	}
+
+	resp := ethKeyPairResponse{
+		PrivateKey: privateKey,
+		Address:    address,
+	}
+
+	ctx.JSONPretty(http.StatusOK, struct {
+		Status string             `json:"status"`
+		Code   int                `json:"code"`
+		Result ethKeyPairResponse `json:"result"`
+	}{
+		Status: "",
+		Code:   http.StatusOK,
+		Result: resp,
+	}, "\t")
+
+	return nil
 }
 
 func (h *handlerEth) GetAddressBalance(ctx *echo.Context) {
