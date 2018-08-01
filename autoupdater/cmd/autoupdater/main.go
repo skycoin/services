c6112ae0d7584d8902ad8a32c67e066c70a144b5
Ivan Costa Lacuesta <icosta@tribeclick.com>
2018-08-01 14:54:06 +0800

    added and documented docker image

--- autoupdater/cmd/autoupdater/main.go
+++ autoupdater/cmd/autoupdater/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"flag"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/skycoin/services/autoupdater/config"
-	"github.com/skycoin/services/autoupdater/src/starter"
+"flag"
+"os"
+"os/signal"
+"syscall"
+
+"github.com/skycoin/services/autoupdater/config"
+"github.com/skycoin/services/autoupdater/src/starter"
+
 )
 
 var configFile string
@@ -17,7 +18,7 @@ func main() {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	flag.StringVar(&configFile,"config", "../configuration.yml", "yaml configuration file")
+	flag.StringVar(&configFile,"config", "./configuration.yml", "yaml configuration file")
 	flag.Parse()
 
 	configuration := config.New(configFile)
@@ -27,34 +28,3 @@ func main() {
 	s.Stop()
 }
 
-func stringPickNonZero(confValue, flagValue string) string {
-	if confValue == "" {
-		return flagValue
-	}
-
-	return confValue
-}
-
-func stringSlicePickNonZero(confValue, flagValue []string) []string {
-	if confValue == nil {
-		return flagValue
-	}
-
-	return confValue
-}
-
-func intPickNonZero(confValue, flagValue int) int{
-	if confValue == 0 && flagValue != 0 {
-		return flagValue
-	}
-
-	return confValue
-}
-
-func durationPickNonZero(confValue, flagValue time.Duration) time.Duration {
-	if confValue == 0 {
-		return flagValue
-	}
-
-	return confValue
-}
